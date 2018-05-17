2b68f0f89bf288bcad761be0f0ac9122790b07dd
lijiehua <41830859@qq.com>
2018-05-17 12:30:53 +0800

    Initial commit

---
+++ src/main/java/com/muku/docker/MukuDockerApplication.java
@@ -0,0 +1,12 @@
+package com.muku.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MukuDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MukuDockerApplication.class, args);
+    }
+}
